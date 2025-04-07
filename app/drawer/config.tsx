486eab9e15acc61652d147cbc4d90a980fd4ee5b
Jonathanleivag <email@jonathanleivag.cl>
2025-04-07 18:58:25 -0400

    refactor(drawer): replace inline components with dedicated screens
    Move the inline component logic for the About and Config drawer screens into dedicated screen components to improve code organization and maintainability

--- app/drawer/config.tsx
+++ app/drawer/config.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react'
+import ConfigScreen from '../../screens/drawer/config.screen'
 
 const Logout: FC = () => {
-  return null
+  return <ConfigScreen />
 }
 
 export default Logout
